29a2800f530d1725c566d16e4876dfa24aeadb65
Bogdan-Lishchynskiy <lishchynskiy91@gmail.com>
2018-04-18 22:57:45 +0300

    fixed some tasks

--- Easy/1.2/1.2.js
+++ Easy/1.2/1.2.js
@@ -1,16 +1,14 @@
 function maxMinValue(arr) {
-      var min = arr[0],
-      max = min;
-  for (var i = 0; i < arr.length; i++){
-      if(arr[i]<min) {
-          min = arr[i];
-      }else if (arr[i]>max){
-          max = arr[i];
-      }
-  }
-  console.log('Min el = '+min);
-  console.log('Max el = '+max);
+    var min = arr[0],
+        max = min;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        } else if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    console.log('Min el = ' + min);
+    console.log('Max el = ' + max);
 }
-maxMinValue(([1, 92, -5, 25]));
-
-          
+maxMinValue([1, 92, -5, 25]);
--- Haha/0.5/0.5.js
+++ Haha/0.5/0.5.js
@@ -2,13 +2,13 @@ function convertMoney() {
     var regExpOnlyNumbers = /^[0-9]*$/;
     do {
         var usd = +prompt("Enter number to convert USD to UAH:", 33);
-    } while (!regExpOnlyNumbers.test(usd) == true);
+    } while (!regExpOnlyNumbers.test(usd));
     var convert = usd * 27;
     alert(usd + '$' + ' = ' + convert + 'UAH');
 
     do {
         var uah = +prompt("Enter number to convert UAH to USD:", 33);
-    } while (!regExpOnlyNumbers.test(uah) == true);
+    } while (!regExpOnlyNumbers.test(uah));
     var conv = uah / 27;
     alert(uah + 'UAH' + ' = ' + conv + '$');
 }
